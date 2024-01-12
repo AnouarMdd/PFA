2a14cd680774603debca012030255c5233793c4e
anouar <anouarelmadidi9@gmail.com>
2024-01-12 17:55:03 +0100

    message

---
+++ src/main/java/com/inn/restaurant/controllers/AuthenticationController.java
@@ -0,0 +1,31 @@
+package com.inn.restaurant.controllers;
+
+import com.inn.restaurant.dto.JwtAuthenticationResponse;
+import com.inn.restaurant.dto.SignInRequest;
+import com.inn.restaurant.dto.SignUpRequest;
+import com.inn.restaurant.services.AuthenticationService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/v1")
+@RequiredArgsConstructor
+public class AuthenticationController {
+
+    private final AuthenticationService authenticationService;
+
+    @PostMapping("/signup")
+    public JwtAuthenticationResponse signup(@RequestBody SignUpRequest request) {
+        return authenticationService.signup(request);
+    }
+
+    @PostMapping("/signin")
+    public JwtAuthenticationResponse signin(@RequestBody SignInRequest request) {
+        return authenticationService.signin(request);
+    }
+}
---
+++ src/main/java/com/inn/restaurant/models/Role.java
@@ -0,0 +1,6 @@
+package com.inn.restaurant.models;
+
+public enum Role {
+    ROLE_ADMIN,
+    ROLE_USER
+}
